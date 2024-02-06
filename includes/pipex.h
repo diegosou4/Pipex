c6423bc335b612a9a6ee4a66d42878cf2c435108
Diego Moreira Sardinha De Souza <diegmore@c2r8s4.42lisboa.com>
2024-02-06 17:18:39 +0000

    (add) include for pipex

---
+++ includes/pipex.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 16:36:21 by diegmore          #+#    #+#             */
+/*   Updated: 2024/02/06 16:36:23 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include "ft_printf.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
