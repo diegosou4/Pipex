05bb939eb2358db39b6993e1ec5f71b348be9a0a
Diego Moreira Sardinha De Souza <diegoaguia31@gmail.com>
2024-02-06 18:26:40 +0000

    (update) add new fuction in header

--- includes/libft.h
+++ includes/libft.h
@@ -13,7 +13,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <unistd.h>
+#include <stdlib.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+void	*ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
