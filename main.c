3a3cd35e6b2f01a4cf5753b8f1873bedb1bca236
Diegosou4 <diegoaguia31@gmail.com>
2024-02-08 10:53:30 +0000

    (test) use new fuction

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: diemorei <diemorei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 16:42:56 by diegmore          #+#    #+#             */
-/*   Updated: 2024/02/06 16:42:57 by diegmore         ###   ########.fr       */
+/*   Updated: 2024/02/08 10:33:50 by diemorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main(int ac, char **av, char **env)
 {
 
     char *path_value;
+    
     path_value = NULL;
     path_value = ft_getenv(env);
     if(path_value == NULL)
@@ -25,11 +26,7 @@ int main(int ac, char **av, char **env)
         return(-1);
     }
     char **arr;
-
     arr = ft_split(path_value,':');
-
-  
-
-
+    av[1] = checkpath(arr, ft_strjoin("/", av[1]));
     return(0);
 }
