26c0976443fe8ec09c266bd46785775e2718a72c
Diegosou4 <diegoaguia31@gmail.com>
2024-02-11 22:00:56 +0000

    (update) now fuction can return the path

--- src/pipex/checkpath.c
+++ src/pipex/checkpath.c
@@ -6,7 +6,7 @@
 /*   By: diemorei <diemorei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 12:26:07 by diegmore          #+#    #+#             */
-/*   Updated: 2024/02/11 13:18:49 by diemorei         ###   ########.fr       */
+/*   Updated: 2024/02/11 14:28:00 by diemorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,21 @@ char *checkpath(char **path, char *command)
 {
     int i = 0;
     char *path_value = NULL;
-
+    char *comandf;
     while(path[i] != NULL)
     {
-        if(access(ft_strjoin(path[i], command), F_OK) == 0)
+        comandf = ft_strjoin("/",command);
+        path_value = ft_strdup(ft_strjoin(path[i], comandf));
+        if(access(path_value, F_OK) == 0)
         {
-            path_value = ft_strdup(ft_strjoin(path[i], command));
+            free(comandf);
             return path_value;
         }
+        free(path_value);
+        free(comandf);
         i++;
     }
-
+    perror("Comando nao encontrado\n");
     return NULL;
 }
 
@@ -51,24 +55,13 @@ void commands(int ac,char **av,t_pipe **pipex)
     }
 }
 
-void parse_progam(int ac,char **av,char **env)
+void parse_progam(int ac,char **av,char **env, t_pipe **pipex)
 {
-    char *path_value;    
-    t_pipe *pipex;
-    pipex = NULL;
-    char *path;
-    path = ft_getenv(env);
-    //  Protecao pro path
+    (*pipex)->path = ft_getenv(env);
 
-
-   // commands(ac,av,&pipex);
-  //  pipex->path = ft_split(path,':');
- //   exec_pipe(&pipex,pipex->path,env);
-
-
-
-    /*
-    execve(path,arg,env);
-    */
-
+    ask_acess(pipex);
+    if((*pipex)->path == NULL)
+    {
+        free(*pipex);        
+    }
 }
