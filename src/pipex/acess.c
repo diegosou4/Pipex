370d6adca071c119e966dbf78c9c7c6edb9c32c7
Diegosou4 <diegoaguia31@gmail.com>
2024-02-11 22:00:02 +0000

    (update) use struct for do

--- src/pipex/acess.c
+++ src/pipex/acess.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   acess.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: diemorei <diemorei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 15:47:29 by diegmore          #+#    #+#             */
-/*   Updated: 2024/02/08 15:47:31 by diegmore         ###   ########.fr       */
+/*   Updated: 2024/02/11 14:28:58 by diemorei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,13 @@
 #include "../../includes/pipex.h"
 
 
-char *ask_acess(t_pipe **pipex,char **path, int indexcom)
+char *ask_acess(t_pipe **pipex)
 {
-   t_pipe *ptr;
-    int i;
-    char *str;
-    i = 0;
-   ptr = *(pipex);
-    while(i != indexcom)
-    {
-        ptr = ptr->next;
-        i++;
-    }
-    while(path[i] != NULL)
-    {
-        str = ft_strjoin("/",ptr->comands[0]);
-        str = ft_strjoin(path[i] ,str);
-        if(access(str,F_OK) == 0)
-        {
-            return(str);
-        }
-        i++;
-    }
-    return(NULL);
+
+   char **paths;
+
+   paths = ft_split((*pipex)->path, ':');
+   free((*pipex)->path);
+   (*pipex)->path = checkpath(paths, "ls");
 }
 
