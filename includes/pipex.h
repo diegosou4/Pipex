ead306a9746e198641b7fadb9a0b8aee0b20538c
Diego Moreira Sardinha De Souza <diegoaguia31@gmail.com>
2024-02-12 17:15:50 +0000

    (update) new fuctions

--- includes/pipex.h
+++ includes/pipex.h
@@ -43,17 +43,20 @@ char *checkpath(char **path, char *command);
 
 void exec(char *path, char **args, int in, int out, char **env);
 int check_ac(int ac);
-void exec_pipe(t_pipe **pipex, char **paths, char **env);
+
 
 void printf_error(char *str);
 void parse_progam(int ac,char **av,char **env, t_pipe **pipex);
 char *simple_split(char *str, char sep);
-void exec_pipe(t_pipe **pipex, char **paths, char **env);
+void exec_pipe(t_pipe **pipex,char **env);
 void openfd(t_pipe *pipex,int ac,char **av);
 void *ask_acess(t_pipe **pipex, char *path);
 char *checkpath(char **path, char *command);
 
 t_cmd *new_cmd(char *comands);
+void free_commands(t_cmd **comands, t_pipe **pipex);
 void add_backcmd(char *comands, t_cmd **cmd);
+
+void closefd(t_pipe **pipex);
 
 #endif
